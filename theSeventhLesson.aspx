a19e86f5f56d0fcf9f2caa6746b88d063d96fa7a
YagamiAllice <47944549+YagamiAllice@users.noreply.github.com>
2019-04-05 15:39:14 +0800

    Add files via upload

---
+++ theSeventhLesson.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="theSeventhLesson.aspx.cs" Inherits="ProjectEchart.theSeventhLesson" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        请输入一个年份<asp:TextBox ID="txtYear" runat="server"></asp:TextBox>
+    
+        <asp:Button ID="btnCheckYear" runat="server" OnClick="btnCheckYear_Click" Text="检测是否为闰年" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ theSeventhLesson.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectEchart
+{
+    public partial class theSeventhLesson : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //如何向数据库中插入数据
+            //isleapyear(y)
+        }
+            private bool isLeapYear(double a)
+            {
+               double year=double.Parse(txtYear.Text);
+               if (year % 4 == 0&&year%100 != 0|| year % 400 == 0)
+               {
+                   a = 1;
+                   bool b = true;
+                   Response.Write("是闰年");
+                   return b;
+               }
+               else
+               {
+                   a = 0;
+                   bool b = false;
+                   Response.Write("不是闰年");
+                   return b;
+               }
+            }
+        protected void btnCheckYear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double y = double.Parse(txtYear.Text);
+                bool c = isLeapYear(y);
+            }
+            catch
+            {
+                Response.Write("请输入阿拉伯数字格式的年份");
+            }
+        }
+    }
+}
